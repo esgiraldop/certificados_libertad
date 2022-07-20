bbaf8b99201d1831be6bce1fa6bcb16218b8de35
Erick Giraldo <erickgiraldo94@gmail.com>
2022-07-20 16:07:04 -0500

    Created script make_analysis

--- CT.py
+++ CT.py
@@ -11,62 +11,7 @@ import os
 import read_pdf
 import parse_pdf
 import codgs_juridics
-    
-def make_analysis(info_df, loglist):
-    '''Function to evaluate if the document is approved or not.
-            - Returns "ERROR EN EL ANALISIS" if there are "Cod. especs." that could not be found in the
-                database.
-            - Returns "REVISION" if the amount of "ABRE" + "LIMITA" is greater than 
-                "CANCELA" in the column "Tipo" of "info_df".
-            - Returns "APROBADO" otherwise.
-            
-            '''
-    
-    if info_df['Tipo'].isnull().any():
-        # If the CT has any 'Cod. espec.' that could not be found in the database, skip this function
-        return 'ERROR EN EL ANALISIS'
-    
-    Tipo_counts = pd.Series([0,0,0,0,0],index=['ABRE', 'LIMITA', 'CANCELA', 'INFORMA', 'A REVISION'])
-    info_df_types = info_df['Tipo'].value_counts()
-    
-    # taking into account that an "anotación" can cancell multiple other "anotaciones" as well
-    num_cancelaciones_totales = 0
-    for index, item in info_df['Cancela anotacion'].iteritems():
-        if type(item) == type(list()):
-            num_cancelaciones_totales += len(item)   
-    info_df_types.loc['CANCELAN'] = num_cancelaciones_totales
-    
-    for info_df_type, amount in info_df_types.iteritems():
-        Tipo_counts.loc[info_df_type] = amount
-        
-        
-    if 'A REVISION' in info_df_types.index and info_df_types['A REVISION'] > 0: # if there is any "anotacion" with "A REVISION", the document is sent to be reviewed by the lawers
-        a_revision_anots = info_df['No anotacion'][info_df['Tipo'] == 'A REVISION'].values
-        msg = 'El documento debe enviarse a revisión. Revisar cuidadosamente las anotaciones:\n' + ', '.join(a_revision_anots)
-        loglist.append(msg)
-        loglist.append('\n')
-        print("El documento debe enviarse a revisión. Revisar cuidadosamente las anotaciones: ", *a_revision_anots, sep='\n')
-        print("\n")
-        return 'REVISION'
-    elif Tipo_counts.loc['ABRE'] + Tipo_counts.loc['LIMITA'] > Tipo_counts.loc['CANCELA']:
-        msg = 'Hay más aperturas y limitaciones que cancelaciones. El documento debe ir a revisión\nAnálisis exitoso\n\n'
-        loglist.append(msg)
-        # loglist.append('\n')
-        print(msg)
-        return 'REVISION'
-    elif Tipo_counts.loc['ABRE'] + Tipo_counts.loc['LIMITA'] < Tipo_counts.loc['CANCELA']:
-        msg = 'Hay más cancelaciones que aperturas y limitaciones. El documento debe ir a revisión\nAnálisis exitoso\n\n'
-        loglist.append(msg)
-        loglist.append('\n')
-        print(msg)
-        return 'REVISION'
-    else:
-        msg = 'El documento está aprobado\nAnálisis exitoso\n\n'
-        loglist.append(msg)
-        print(msg)
-        loglist.append('\n')
-        return 'APROBADO'
-
+import make_analysis
 
 def init_choice_func():
     '''
@@ -352,7 +297,7 @@ def iterator(filepath, file):
         certificate_analysis = pd.DataFrame(columns = ['no matricula', 'Nombre_archivo', 'Aprobado_revision'])
         certificate_analysis.loc[0,'no matricula'] = info_df.iloc[0,0]
         certificate_analysis.loc[0,'Nombre_archivo'] = info_df.iloc[0,1]
-        certificate_analysis.loc[0,'Aprobado_revision'] = make_analysis(info_df, loglist)
+        certificate_analysis.loc[0,'Aprobado_revision'] = make_analysis.main(info_df, loglist)
     
         certificates_info_df = pd.concat([certificates_info_df, info_df])
         certificates_analysis = pd.concat([certificates_analysis, certificate_analysis])

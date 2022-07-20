a41dce15a5744551258fe833a0b5fa055eaf8133
Erick Giraldo <erickgiraldo94@gmail.com>
2022-07-19 21:01:07 -0500

    Created file read_pdf.py

--- CT.py
+++ CT.py
@@ -8,69 +8,10 @@ Created on Sat Oct 17 21:44:04 2020
 import numpy as np
 import pandas as pd
 import os
-import camelot
 import datetime as dt
 import re
 import time
-
-def readPDF(fullname, filename, loglist):
-    
-    '''Function for reading a pdf with camelot and storing the certificado de libertad
-        in a dataframe'''
-    
-    CT2parse = camelot.handlers.PDFHandler(fullname, pages='all')
-    certificate_table = camelot.read_pdf(fullname, pages='all', flavor='stream',
-                                         split_text = False, edge_tol=10,
-                                         suppress_stdout=True)
-    # extracting what pages have 5 rows or less. (it means it is a scanned document)
-    page_lengths = []
-    for page in certificate_table:
-        page_lengths.append(len(page.df))
-    filtered_lengths = list(filter(lambda page_length: page_length <= 5, page_lengths))
-   
-    if certificate_table.n == 0 or len(filtered_lengths) > 0:
-        msg = f'Hubo un error leyendo el archivo \'{filename}\'. Por favor revise que no contenga imágenes.'
-        loglist.append(msg)
-        print(msg)
-        loglist.append('\n')
-        return None        
-    elif certificate_table.n < len(CT2parse.pages):
-        # If there are less parsed pages than the actual pages, probably there are
-            # some that are image-based (e.g. scanned docs) instead of text-based pages
-        msg = f'Hubo un error leyendo una de las páginas del archivo \'{filename}\'. Por favor revise que ninguna de las páginas contenga imágenes.'
-        loglist.append(msg)
-        print(msg)
-        loglist.append('\n')
-        return None
-
-    certificate_df = pd.DataFrame()
-    
-    for i in range(certificate_table.n):
-        certificate_df = pd.concat([certificate_df, certificate_table[i].df], axis=0)
-       
-    certificate_df.reset_index( drop=True, inplace=True)
-    
-    # After checking the PDF has not any image-based page, it is necessary to check
-        # whether the document is actually a CT or not
-
-    for item in certificate_df.itertuples():
-        # print(item)
-        # print(item._2)
-        
-        for elem in item:
-        
-            if elem == 'CERTIFICADO DE TRADICION':
-                # print('Es un CT')
-                return certificate_df
-        
-        if item.Index > 10: # No need of going throughout the document
-            break
-
-    msg = 'El documento no es un Certificado de tradición. Se omite el análisis del documento.'
-    loglist.append(msg)
-    print(msg)
-    print('\n')
-    return None
+import read_pdf
     
 def parsePDF(certificate_df, filename, loglist):
     
@@ -108,7 +49,8 @@ def parsePDF(certificate_df, filename, loglist):
     loglist.append('\n')
     print(msg)
     # if delta_time.days > 90:
-        # return 'too old'
+        # return
+    # 'too old'
     
     
     # Extracting the number of the pin of the certificate
@@ -659,7 +601,7 @@ def iterator(filepath, file):
         loglist.append('\n')
 
         try:
-            certificate_df = readPDF(filepath+'\\'+filename, filename, loglist)
+            certificate_df = read_pdf.main(filepath+'\\'+filename, filename, loglist)
             if np.all(certificate_df == None):
                 # Notifying the error in excel
                 certificate_analysis = writeError2excel(filename)
---
+++ read_pdf.py
@@ -0,0 +1,59 @@
+import camelot
+
+def main(fullname, filename, loglist):
+    '''Function for reading a pdf with camelot and storing the certificado de libertad
+        in a dataframe'''
+
+    CT2parse = camelot.handlers.PDFHandler(fullname, pages='all')
+    certificate_table = camelot.read_pdf(fullname, pages='all', flavor='stream',
+                                         split_text=False, edge_tol=10,
+                                         suppress_stdout=True)
+    # extracting what pages have 5 rows or less. (it means it is a scanned document)
+    page_lengths = []
+    for page in certificate_table:
+        page_lengths.append(len(page.df))
+    filtered_lengths = list(filter(lambda page_length: page_length <= 5, page_lengths))
+
+    if certificate_table.n == 0 or len(filtered_lengths) > 0:
+        msg = f'Hubo un error leyendo el archivo \'{filename}\'. Por favor revise que no contenga imágenes.'
+        loglist.append(msg)
+        print(msg)
+        loglist.append('\n')
+        return None
+    elif certificate_table.n < len(CT2parse.pages):
+        # If there are less parsed pages than the actual pages, probably there are
+        # some that are image-based (e.g. scanned docs) instead of text-based pages
+        msg = f'Hubo un error leyendo una de las páginas del archivo \'{filename}\'. Por favor revise que ninguna de las páginas contenga imágenes.'
+        loglist.append(msg)
+        print(msg)
+        loglist.append('\n')
+        return None
+
+    certificate_df = pd.DataFrame()
+
+    for i in range(certificate_table.n):
+        certificate_df = pd.concat([certificate_df, certificate_table[i].df], axis=0)
+
+    certificate_df.reset_index(drop=True, inplace=True)
+
+    # After checking the PDF has not any image-based page, it is necessary to check
+    # whether the document is actually a CT or not
+
+    for item in certificate_df.itertuples():
+        # print(item)
+        # print(item._2)
+
+        for elem in item:
+
+            if elem == 'CERTIFICADO DE TRADICION':
+                # print('Es un CT')
+                return certificate_df
+
+        if item.Index > 10:  # No need of going throughout the document
+            break
+
+    msg = 'El documento no es un Certificado de tradición. Se omite el análisis del documento.'
+    loglist.append(msg)
+    print(msg)
+    print('\n')
+    return None
